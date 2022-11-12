f63c0b0bc9d7ae075aac4983c4993b5264489717
Jaeseong Lee <jason.lee27@utexas.edu>
2022-11-11 21:44:10 -0600

    run model

--- src/salary/python/models/RunModel.py
+++ src/salary/python/models/RunModel.py
@@ -45,55 +45,59 @@ class RunModel:
 
     @classmethod
     def get_model_test_accuracy(cls, model_dict: Dict, x_test, y_test):
-        result_str = ''
+        result_str = 'model_name,accuracy\n'
         for model_name, model in model_dict.items():
             acc = model.test_models(x_train, y_train)
-            result_str += f"{model_name}\t{acc}\n"
+            result_str += f"{model_name},{acc}\n"
         # end for
 
         # TODO: save result here?
-        return result_str
+        save_dir = Macros.result_dir / 'salary'
+        save_dir.mkdir(parents=True, exist_ok=True)
+        Utils.write_txt(result_str, save_dir / 'test_accuracy.csv')
+        return
 
     @classmethod
     def get_confusion_matrices(cls, model_dict: Dict, x_test, y_test):
-        result_str = ''
+        result_str = 'model_name,tn,fp,fn,tp\n'
         for model_name, model in model_dict.items():
             # Compute the test error
             y_pred = [predict_example(x, decision_tree) for x in Xtst]
             tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-            result_str += f"decision_tree::depth_{d}::confusion_matrix::tn, fp, fn, tp = {tn}, {fp}, {fn}, {tp}\n"
+            result_str += f"{model_name},{tn},{fp},{fn},{tp}\n"
         # end for
-        return result_str
+        save_dir = Macros.result_dir / 'salary'
+        save_dir.mkdir(parents=True, exist_ok=True)
+        Utils.write_txt(result_str, save_dir / 'confusion_matrix.csv')
+        return
 
     @classmethod
     def get_feature_importance(cls, model_dict: Dict):
-        # TODO
-        st = time.time()
+        sns.set_theme()
         for model_name, model in model_dict.items():
-            importances = model.feature_importances_
-
-
+            # feature importance: ndarray of shape (n_features,)
+            feat_importances = model.feature_importances_
+            data_lod = list()
+            for f_i in range(len(feat_importances)):
+                data_lod.append({
+                    'feat_id': f_i+1,
+                    'feat_importance': feat_importances[f_i]
+                })
+            # end for
             df: pd.DataFrame = pd.DataFrame.from_dict(Utils.lod_to_dol(data_lod))
 
             # Plotting part
             fig: plt.Figure = plt.figure()
             ax: plt.Axes = fig.subplots()
-            ax = sns.scatterplot(data=df,
-                                 x='feat_x',
-                                 y='feat_y',
-                                 hue=group_by,
-                                 ax=ax,
-                                 palette='Paired')
-            ax.set_xlabel('feat-x')
+            ax = sns.barplot(data=df,
+                             x='feat_id',
+                             y='feat_importance',
+                             ax=ax,
+                             palette='Paired')
+            ax.set_xlabel('features')
             ax.set_ylabel('feat-y')
-            
-            forest_importances = pd.Series(importances, index=feature_names)
-            
-            fig, ax = plt.subplots()
-            forest_importances.plot.bar(yerr=std, ax=ax)
-            ax.set_title("Feature importances using MDI")
-            ax.set_ylabel("Mean decrease in impurity")
             fig.tight_layout()
+            fig.savefig(figs_dir / f"feature_importance_{model_name}_barplot.eps")
         # end for
         return
 
